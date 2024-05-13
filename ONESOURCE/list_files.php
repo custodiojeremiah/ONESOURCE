6880ade2031e9fc243649891fc8b743e5385cb74
custodiojeremiah <163700224+custodiojeremiah@users.noreply.github.com>
2024-05-14 07:08:25 +0800

    Delete ONESOURCE directory

--- ONESOURCE/list_files.php
+++
@@ -1,8 +0,0 @@
-<?php
-$files = scandir("uploads/");
-foreach($files as $file) {
-    if ($file != "." && $file != "..") {
-        echo "<li><a href='uploads/$file' download>$file</a></li>";
-    }
-}
-?>
